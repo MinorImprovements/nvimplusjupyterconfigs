3256827162a08598e3a511a2cc08eb5aa38ceb78
welkerxa <welkerxa@amazon.com>
2026-03-23 20:43:00 -0700

    complete parts of the guide that I felt were usefull and updated some depriciated configs from it

---
+++ init.lua
@@ -0,0 +1,11 @@
+require "options"
+require "launch"
+spec("plugins.colorscheme")
+spec("plugins.treesitter")
+spec("plugins.telescope")
+spec("plugins.fugitive")
+spec("plugins.harpoon")
+spec("plugins.lsp-zero")
+spec("plugins.mason")
+require "plugins.lazy"
+require "keymaps"
---
+++ lua/keymaps.lua
@@ -0,0 +1,48 @@
+--project view
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- telescope keymaps
+
+local builtin = require "telescope.builtin"
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-g>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+   builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+
+--fugitive.lua keymaps
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- harpoon keymaps
+local mark = require "harpoon.mark"
+local ui = require "harpoon.ui"
+local term = require "harpoon.term"
+vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, {})
+vim.keymap.set("n", "<leader>a", mark.add_file, {})
+vim.keymap.set("n", "<C-j>", ui.nav_next, {})
+vim.keymap.set("n", "<C-k>", ui.nav_prev, {})
+vim.keymap.set("n", "<C-t>", function()
+   term.gotoTerminal(1)
+end)
+
+-- lsp keymaps
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol)
+vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references)
+vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename)
+
+-- lsp keymaps
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol)
+vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references)
+vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename)
